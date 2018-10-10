9ff621d4d2cd64c2ba600787e4a50a8c995c25c7
Lisa Hamberg (JMD676) <lisa.z.hamberg@teliacompany.com>
2018-10-10 15:55:46 +0200

    init commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <movie-manager/>npm
+  </div>
+</template>
+
+<script>
+import MovieManager from './components/Dashboard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MovieManager
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<script>
+    import Header from "./Header";
+    import axios from "axios";
+    export default {
+        name: "SignUp",
+        components: {
+            Header
+        },
+        data() {
+            return {
+                model: {
+                    name: "",
+                    email: "",
+                    password: "",
+                    c_password: "",
+                    company_name: ""
+                },
+                loading: "",
+                status: ""
+            };
+        }
+    }
+</script>
